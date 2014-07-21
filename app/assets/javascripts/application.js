f643e38a24fb3972bc7a9162d7df6f0e0ee1cd29
yafeilee <lyfi2003@gmail.com>
2014-07-21 15:01:40 +0800

    foundation code update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation/foundation
+//= require foundation/foundation.topbar
 //= require foundation/foundation.offcanvas
 //= require angularjs
 //= require 'jquery.html5-fileupload'
