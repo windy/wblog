740b6c0eced4869fb8cd08e347775dab55ed2bc7
yafeilee <lyfi2003@gmail.com>
2015-04-08 09:20:44 +0800

    add zh to default deploy destination, fix a bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
+set :stages, %w(en zh)
+set :default_stage, 'zh'
+
 require 'mina/multistage'
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 require 'mina/rvm'    # for rvm support. (http://rvm.io)
 require 'mina/unicorn'
-
-set :stages, %w(en zh)
-set :default_stage, 'zh'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
