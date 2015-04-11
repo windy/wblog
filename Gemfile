62d1f394a30a88fe90647bccbf788e1e864aab93
yafeilee <lyfi2003@gmail.com>
2015-04-12 00:59:38 +0800

    add mina sidekiq

--- Gemfile
+++ Gemfile
@@ -38,7 +38,8 @@ gem 'newrelic_rpm'
 
 gem 'mina', require: false
 gem 'mina-multistage', require: false
-gem 'mina-unicorn', :require => false
+gem 'mina-sidekiq', require: false
+gem 'mina-unicorn', require: false
 
 group :development do
   gem 'spring'
--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ require 'mina/rails'
 require 'mina/git'
 require 'mina/rvm'    # for rvm support. (http://rvm.io)
 require 'mina/unicorn'
+require 'mina_sidekiq/tasks'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
@@ -47,6 +48,7 @@ task :deploy => :environment do
 
     to :launch do
       invoke :'unicorn:restart'
+      invoke :'sidekiq:restart'
     end
   end
 end
