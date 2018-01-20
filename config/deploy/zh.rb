9bf7ad5b6d0c374589cf61a72d1c6fba732f632c
yafeilee <lyfi2003@gmail.com>
2018-01-21 02:41:55 +0800

    update profile

--- config/deploy/zh.rb
+++ config/deploy/zh.rb
@@ -1,5 +1,5 @@
 set :domain, 'yafeilee.me'
-set :deploy_to, '/home/ruby/wblog'
+set :deploy_to, '/data/www/wblog'
 set :repository, 'git@github.com:windy/wblog.git'
 set :branch, 'master'
 set :user, 'ruby'
