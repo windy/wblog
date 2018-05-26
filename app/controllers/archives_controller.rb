a2873718bb058e70d1e1caa304dd4927b031eaf7
yafeilee <lyfi2003@gmail.com>
2018-05-26 18:31:22 +0800

    add blog search feature

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,5 +1,10 @@
 class ArchivesController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc).page(params[:page])
+    if (@q = params[:q]).blank?
+      @posts = Post.order(created_at: :desc).page(params[:page])
+    else
+      @q_size = Post.where('title like ?', "%#{@q}%").size
+      @posts = Post.where('title like ?', "%#{@q}%").order(created_at: :desc).page(params[:page])
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
   # some format function is defined in app/controllers/application_controller.rb
+  #
+  def search_highlight(title, q)
+    return title if q.blank?
+
+    title.sub(q, "<em>#{q}</em>")
+  end
 end
