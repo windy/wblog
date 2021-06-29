08dfe9c94ca6d20c5fc6066eeff5ec4035ccb958
windy <yafei@dao42.com>
2021-06-29 15:16:01 +0800

    add animate to about page

---
+++ app/javascript/js/about.js
@@ -0,0 +1,21 @@
+import '../js/libs/ddscrollspy'
+
+$(document).on('turbo:load', function(){
+  if($('.pages-about-page').length === 0) { return; }
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0)
+      $('.top-bar-wrapper').addClass('active')
+    else
+      $('.top-bar-wrapper').removeClass('active')
+  })
+
+  $('#about-top-bar').ddscrollSpy({highlightclass: 'active'})
+
+  $('#about-anchor-link').click(function(e){
+    e.preventDefault()
+    $('html, body').animate({
+      scrollTop: $('#about').offset().top
+    },500)
+  })
+})
