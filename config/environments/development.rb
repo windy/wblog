4df24e60a0ecc28917947d8e79a9497e4ab35149
windy <yafei@dao42.com>
2025-02-24 20:40:38 +0800

    chore: improve dev speed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
   # Do not eager load code on boot.
   config.eager_load = false
 
